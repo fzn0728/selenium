7bd6e7c465dae2487d1e9209711e5a3afd9674c7
fzn0728 <thames.ft@gmail.com>
2016-12-14 16:58:57 -0800

    MSCI index

--- MSCI.py
+++ MSCI.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def find_hidden_link(url):
+def MSCI_find_hidden_link(url):
     '''
     Find the hidden link given by text indicator "page:", in case that they change the link
     '''
@@ -16,15 +16,14 @@ def find_hidden_link(url):
                         'please check the html file and find the hidden link manually.')
     return link[1:-2]
     
-def get_soup(url_table):
-    # url_table = 'https://app2.msci.com/webapp/indexperf/pages/IEIPerformanceRegional.jsf'
-    soup = BeautifulSoup(urlopen(url_table).read(),'lxml')
-    return soup
 
-def get_value():
+def MSCI_get_value(url_table,MKT):
     '''
     Find 'tbody' first, and locate the 'tr' tag, then extract all text after 'td' tag.
     '''
+    # Get soup
+    soup = BeautifulSoup(urlopen(url_table).read(),'lxml')
+    # Get data value
     data = []
     table = soup.find("tbody", id = "templateForm:tableResult0:tbody_element")
     rows = table.find_all('tr')
@@ -32,18 +31,23 @@ def get_value():
         cols = tr.find_all('td')
         cols = [ele.text.strip() for ele in cols]
         data.append([ele for ele in cols if ele]) # Get rid of empty values    
-    return data
+    df = pd.DataFrame([part for part in data])
+    df.columns=['MSCI Index','Index Code','Last','Day','MTD','3MTD','YTD','1 Yr','3 Yr','5 Yr','10 Yr']
+    df.to_csv('MSCI_%s.csv'%MKT)
+    
+    return df
 
+    
 
 if __name__ == "__main__":
     # Get the hidden link
-    url = 'https://www.msci.com/end-of-day-data-search'
-    link = find_hidden_link(url)
+    hidden_link_DM = 'https://app2.msci.com/webapp/indexperf/pages/IEIPerformanceRegional.jsf'
+    hidden_link_EM = 'https://app2.msci.com/webapp/indexperf/pages/IEIPerformanceRegional.jsf'
+    ### link = find_hidden_link(url)
     # Fetch html from url (jsf in this case)
-    soup = get_soup(link)
     # Parse raw data from the table 
-    data = get_value()
+    data_DM_df = MSCI_get_value(hidden_link_DM,'DM')
+    data_EM_df = MSCI_get_value(hidden_link_EM,'EM')
     # Clean the format and change it into dataframe
-    df = pd.DataFrame([part for part in data])
-    df.columns=['MSCI Index','Index Code','Last','Day','MTD','3MTD','YTD','1 Yr','3 Yr','5 Yr','10 Yr']
-    df.to_csv('MSCI_table.csv')
+    
+    
